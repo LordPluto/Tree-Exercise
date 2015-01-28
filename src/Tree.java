56367f45f060f301d403de23f735f7f6413a8e40
Shannon Weir <stw3763@rit.edu>
2015-01-28 18:50:23 -0500

    Added Comments
    Added the comments for the various functions in Tree.java.
    
    ~ Added comments to the functions

--- src/Tree.java
+++ src/Tree.java
@@ -20,6 +20,12 @@ public class Tree {
 		}
 	}
 	
+	/**
+	 * Builds the layer given by target depth recursively.
+	 * @param currentDepth
+	 * @param targetDepth
+	 * @param currentNode
+	 */
 	private void BuildLayer (int currentDepth, int targetDepth, Node currentNode){
 		if(currentDepth == targetDepth - 1){		//We found our target depth.
 			Node leftNeighbor = getLeftNeighbor(currentNode);
@@ -27,7 +33,8 @@ public class Tree {
 				Node newNode = new Node(currentNode.getData());
 				newNode.setParent(currentNode);
 				currentNode.setLeftChild(newNode);
-			} else {
+			}
+			else {
 				Node newNode = new Node(currentNode.getData() + leftNeighbor.getData());
 				newNode.setParent(currentNode);
 				currentNode.setLeftChild(newNode);
@@ -38,28 +45,37 @@ public class Tree {
 				Node newNode = new Node(currentNode.getData());
 				newNode.setParent(currentNode);
 				currentNode.setRightChild(newNode);
-			} else {
+			}
+			else {
 				Node newNode = new Node(currentNode.getData() + rightNeighbor.getData());
 				newNode.setParent(currentNode);
 				currentNode.setRightChild(newNode);
 			}			
-		} else {
+		}
+		else {
 			if(currentNode.getLeftChild() != null && currentNode.getRightChild() != null){
 				BuildLayer(currentDepth+1, targetDepth, currentNode.getLeftChild());
 				BuildLayer(currentDepth+1, targetDepth, currentNode.getRightChild());
-			} else {
+			}
+			else {
 				System.err.println("Error found. Something didn't get set properly.");
 			}
 		}
 	}
 	
+	/**
+	 * Gets the current Node's parent's left neighbor.
+	 * @param currentNode
+	 * @return
+	 */
 	private Node getLeftNeighbor (Node currentNode) {
 		int rowsUp = 0;
 		while(currentNode.getParent() != null){
 			if(currentNode.getParent().getLeftChild() == currentNode){
 				currentNode = currentNode.getParent();
 				++rowsUp;
-			} else if(rowsUp != 0){
+			}
+			else if(rowsUp != 0){
 				currentNode = currentNode.getParent().getLeftChild();
 				--rowsUp;
 				while(rowsUp >= 0){
@@ -67,7 +83,8 @@ public class Tree {
 					--rowsUp;
 				}
 				return currentNode;
-			}	else {
+			}
+			else {
 				return currentNode.getParent().getLeftChild();
 			}
 		}
@@ -75,13 +92,19 @@ public class Tree {
 		return null;
 	}
 	
+	/**
+	 * Gets the current Node's parent's right neighbor
+	 * @param currentNode
+	 * @return
+	 */
 	private Node getRightNeighbor (Node currentNode) {
 		int rowsUp = 0;
 		while(currentNode.getParent() != null){
 			if(currentNode.getParent().getRightChild() == currentNode){
 				currentNode = currentNode.getParent();
 				++rowsUp;
-			} else if(rowsUp != 0){
+			}
+			else if(rowsUp != 0){
 				currentNode = currentNode.getParent().getRightChild();
 				--rowsUp;
 				while(rowsUp >= 0){
@@ -89,7 +112,8 @@ public class Tree {
 					--rowsUp;
 				}
 				return currentNode;
-			}	else {
+			}
+			else {
 				return currentNode.getParent().getRightChild();
 			}
 		}
@@ -97,12 +121,21 @@ public class Tree {
 		return null;
 	}
 	
+	/**
+	 * Prints the tree in pre-order form. That is, center-left-right
+	 * @return
+	 */
 	public String printTree () {
 		return  origin.getData() + "-" + 
 					printTree(origin.getLeftChild()) + "-" + printTree(origin.getRightChild());
 	}
 	
-	public String printTree (Node currentNode) {
+	/**
+	 * Private string used to recursively print the tree.
+	 * @param currentNode
+	 * @return
+	 */
+	private String printTree (Node currentNode) {
 		if(currentNode.getLeftChild() != null && currentNode.getRightChild() != null){
 			return currentNode.getData() + "-" +
 					printTree(currentNode.getLeftChild()) + "-" + printTree(currentNode.getRightChild());
