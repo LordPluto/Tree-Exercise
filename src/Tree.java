682c6e0828d19db4c2e5efeceb7c1fd229aff08c
Shannon Weir <stw3763@rit.edu>
2015-01-28 15:15:54 -0500

    Integer-based Indexing
    Preparing the skeleton for integer-based indexing Tree instead of
    LinkedList-based Tree.

--- src/Tree.java
+++ src/Tree.java
@@ -1,99 +1,29 @@
+import java.util.*;
 
 public class Tree {
-	private Node origin;
-	
-	private int treeDepth;
-	
-	public Tree () {
-		origin = new Node();
+	private ArrayList<Node> treeNodes;
 		
-		treeDepth = 1;
+	public Tree () {
+		treeNodes = new ArrayList<Node>();
 	}
 	
 	public Tree (int depth) {
-		origin = new Node();
+		treeNodes = new ArrayList<Node>();
 		
-		treeDepth = depth;
-		
-		for(int i = 2;i<=treeDepth;i++){
-			BuildLayer(1,i,origin);
+		for(int i = 0;i<depth;i++){
+			BuildLayer(i);
 		}
 	}
 	
-	private void BuildLayer (int currentDepth, int targetDepth, Node currentNode){
-		if(currentDepth == targetDepth - 1){		//We found our target depth.
-			Node leftNeighbor = getLeftNeighbor(currentNode);
-			if(leftNeighbor == null){
-				Node newNode = new Node(currentNode.getData());
-				newNode.setParent(currentNode);
-				currentNode.setLeftChild(newNode);
-			} else {
-				Node newNode = new Node(currentNode.getData() + leftNeighbor.getData());
-				newNode.setParent(currentNode);
-				currentNode.setLeftChild(newNode);
-			}
-			
-			Node rightNeighbor = getRightNeighbor(currentNode);
-			if(rightNeighbor == null){
-				Node newNode = new Node(currentNode.getData());
-				newNode.setParent(currentNode);
-				currentNode.setRightChild(newNode);
-			} else {
-				Node newNode = new Node(currentNode.getData() + leftNeighbor.getData());
-				newNode.setParent(currentNode);
-				currentNode.setRightChild(newNode);
-			}			
-		} else {
-			if(currentNode.getLeftChild() != null && currentNode.getRightChild() != null){
-				BuildLayer(currentDepth+1, targetDepth, currentNode.getLeftChild());
-				BuildLayer(currentDepth+1, targetDepth, currentNode.getRightChild());
-			} else {
-				System.err.println("Error found. Something didn't get set properly.");
-			}
-		}
+	private void BuildLayer (int currentLayer){
+		
 	}
 	
-	private Node getLeftNeighbor (Node currentNode) {
-		int rowsUp = 0;
-		while(currentNode.getParent() != null){
-			if(currentNode.getParent().getLeftChild() == currentNode){
-				currentNode = currentNode.getParent();
-				++rowsUp;
-			} else if(rowsUp != 0){
-				currentNode = currentNode.getParent().getLeftChild();
-				--rowsUp;
-				while(rowsUp >= 0){
-					currentNode = currentNode.getRightChild();
-					--rowsUp;
-				}
-				return currentNode;
-			}	else {
-				return currentNode.getParent().getLeftChild();
-			}
-		}
-		
+	private Node getLeftNeighbor (int currentPosition) {
 		return null;
 	}
 	
-	private Node getRightNeighbor (Node currentNode) {
-		int rowsUp = 0;
-		while(currentNode.getParent() != null){
-			if(currentNode.getParent().getRightChild() == currentNode){
-				currentNode = currentNode.getParent();
-				++rowsUp;
-			} else if(rowsUp != 0){
-				currentNode = currentNode.getParent().getRightChild();
-				--rowsUp;
-				while(rowsUp >= 0){
-					currentNode = currentNode.getLeftChild();
-					--rowsUp;
-				}
-				return currentNode;
-			}	else {
-				return currentNode.getParent().getRightChild();
-			}
-		}
-		
+	private Node getRightNeighbor (int currentPosition) {		
 		return null;
 	}
 }
